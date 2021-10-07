1802f6f10a16fde6af2b8fcd6d822dd2f7b28cd6
Saad Saiyed <saadsaiyed7@gmail.com>
2021-10-06 21:45:47 -0400

    Going back to initial commit

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 import './style.css'
-import { AxesHelper, BufferAttribute, DirectionalLight, DirectionalLightHelper, DoubleSide, FlatShading, Mesh, MeshBasicMaterial, MeshPhongMaterial, PerspectiveCamera, PlaneGeometry, PointLight, Scene, SphereGeometry, SpotLight, SpotLightHelper, Vector3, WebGLRenderer } from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import { BufferAttribute, DoubleSide, FlatShading, Mesh, MeshPhongMaterial, PerspectiveCamera, PlaneGeometry, PointLight, Scene, WebGLRenderer } from 'three';
+
 
 //Initiate scene - START
     const world = {
-        sphere: {
-            radius: 30,
-            widthSegments:  16,
-            heightSegments: 8,
-            position: new Vector3(0,0,0)
+        plane: {
+            width: 400,
+            height: 400,
+            widthSegments: 50,
+            heightSegments: 50
         },
         light: {
             x: 0,
             y: 0,
-            z: 50,
+            z: 10,
             color: 0x55ff99,
             intensity: 2
         }
@@ -34,110 +34,94 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
     renderer.setPixelRatio(devicePixelRatio);
     document.body.appendChild(renderer.domElement);
     
-    new OrbitControls(camera, renderer.domElement);
-
-    //Sphere - START
-        const sphere = new Mesh(
-            new SphereGeometry(
-                world.sphere.radius,
-                world.sphere.widthSegments,
-                world.sphere.heightSegments
-            ),
-            new MeshPhongMaterial({
-                flatShading: FlatShading,
-                vertexColors: true,
-            })
-        );
-        sphere.position.copy(world.sphere.position)
-        generateSphere();
-        scene.add(sphere);
-    //Sphere - END
-
+    const plane = new Mesh(
+        new PlaneGeometry(
+            world.plane.width,
+            world.plane.height,
+            world.plane.widthSegments,
+            world.plane.heightSegments
+        ),    
+        new MeshPhongMaterial({
+            side: DoubleSide,
+            flatShading: FlatShading,
+            vertexColors: true
+        })
+    );
+    scene.add(plane);
+    generatePlane();
+    
     //Lighting - START
-        const directionalLightTop = new DirectionalLight( 0xFFFFFF, 1);
-        scene.add( directionalLightTop );
-        directionalLightTop.position.set(0, 10, 10);
-
-        const tempLight1 = new DirectionalLight( 0xFFFFFF, 1);
-        tempLight1.position.set(0, -10, -10);
-        scene.add( tempLight1 );
-        
-        const tempLight2 = new DirectionalLight( 0xFFFFFF, 1);
-        tempLight2.position.set(10, 0, -10);
-        scene.add( tempLight2 );
-        
-        const tempLight3 = new DirectionalLight( 0xFFFFFF, 1);
-        tempLight3.position.set(-10, 0, 10);
-        scene.add( tempLight3 );
+        const pointLight = new PointLight(world.light.color, world.light.intensity, 100)
+        pointLight.position.set(
+            world.light.x,
+            world.light.y,
+            world.light.z
+        );
+        scene.add(pointLight);
     //Lighting - END
 
-    //Axis Helper - START
-        // scene.add( new AxesHelper( 500 ) );
-    //Axis Helper - END
-    
+    const mouse = {
+        x: 0,
+        y: 0
+    }
+
 //Initiate scene - END
 
-function generateSphere() {
-    sphere.geometry.dispose();
-    new SphereGeometry(
-        world.sphere.radius,
-        world.sphere.widthSegments,
-        world.sphere.heightSegments
+function generatePlane() {
+    plane.geometry.dispose();
+    plane.geometry = new PlaneGeometry(
+        world.plane.width,
+        world.plane.height,
+        world.plane.widthSegments,
+        world.plane.heightSegments
     )
+    
     // Adding vertices - START
         const randomValues = [];
-        const { array } = sphere.geometry.attributes.position;
+        const { array } = plane.geometry.attributes.position;
         for (let i = 0; i < array.length; i++) {
             if (i % 3 == 0) {
                 const x = array[i]        
                 const y = array[i+1]        
                 const z = array[i+2]        
-                if ((Math.abs(array[i + 1]) == world.sphere.radius) || (i % 17 == 0) || ((i + 3) % 17 == 0) ) {
-                }
-                else {
-                    array[i] = x + (Math.random() - 0.5) * 3
-                    array[i + 1] = y + (Math.random() - 0.5) * 3;
-                    array[i + 2] = z + (Math.random() - 0.5) * 3
-                }
+                
+                array[i] = x + (Math.random() - 0.5) * 3
+                array[i+1] = y + (Math.random() - 0.5) * 3
+                array[i + 2] = z + (Math.random() - 0.5) * 5
             }
+            
             randomValues.push(Math.random() * Math.PI * 2)
         }
-        sphere.geometry.attributes.position.originalPosition = sphere.geometry.attributes.position.array
-        sphere.geometry.attributes.position.randomValues = randomValues
+        plane.geometry.attributes.position.originalPosition = plane.geometry.attributes.position.array
+        plane.geometry.attributes.position.randomValues = randomValues
     // Adding vertices - END
 
     // Vertices Coloring - START
         const colors = []
-        for (let i = 0; i < sphere.geometry.attributes.position.count; i++) {
+        for (let i = 0; i < plane.geometry.attributes.position.count; i++) {
             colors.push(0, 0.19, 0.4)
         }
-        sphere.geometry.setAttribute('color', new BufferAttribute(new Float32Array(colors), 3));
+        plane.geometry.setAttribute('color', new BufferAttribute(new Float32Array(colors), 3));
     // Vertices Coloring - END
 }
 
-var frame = 0;
+let frame = 0 
 function animate() {
+    //perform an animation and requests that the browser calls a specified function to update an animation before the next repaint
     requestAnimationFrame(animate);
-    frame += 0.07;
-    //Moving sphere with individual vertices - START
-        const {array, originalPosition, randomValues} = sphere.geometry.attributes.position
-        for (let i = 0; i < array.length; i+=3) {
-            if ((Math.abs(array[i + 1]) == world.sphere.radius)) {}
-            else if ((i % 17 == 0) || ((i + 3) % 17 == 0)) {
-                array[i] = originalPosition[i] + Math.cos(frame + randomValues[0]) * 0.01
-                array[i+1] = originalPosition[i+1] + Math.sin(frame + randomValues[1]) * 0.01
-                array[i+2] = originalPosition[i+2] + Math.sin(frame + randomValues[2]) * 0.01
-            }
-            else {
-                array[i] = originalPosition[i] + Math.cos(frame + randomValues[i]) * 0.01
-                array[i+1] = originalPosition[i+1] + Math.sin(frame + randomValues[i]) * 0.01
-                array[i+2] = originalPosition[i+2] + Math.sin(frame + randomValues[i]) * 0.01
-            }
-        }
-        sphere.geometry.attributes.position.needsUpdate = true;
-    //Moving sphere with individual vertices - END
+    frame += 0.01;
 
-    sphere.rotation.y += 0.005;
+    //Moving plane with individual vertices - START
+        const {array, originalPosition, randomValues} = plane.geometry.attributes.position
+        for (let i = 0; i < array.length; i+=3) {
+            array[i] = originalPosition[i] + Math.cos(frame + randomValues[i]) * 0.01
+            array[i+1] = originalPosition[i+1] + Math.sin(frame + randomValues[i]) * 0.01
+        }
+        plane.geometry.attributes.position.needsUpdate = true;
+    //Moving plane with individual vertices - END
+
+    //Updating light position relative to mouse position
+    pointLight.position.set(mouse.x , mouse.y, world.light.z);
 
     renderer.render(scene, camera)
 }
@@ -146,6 +130,12 @@ window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
+}, false);
+
+window.addEventListener('mousemove', (event) => {
+    var threshold = 0.105;
+    mouse.x = (event.clientX - (window.innerWidth / 2)) * threshold
+    mouse.y = ((window.innerHeight / 2) - event.clientY) * threshold
 }, false);
 
 animate();
